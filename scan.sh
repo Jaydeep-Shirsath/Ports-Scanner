06be2ca506fd2c3ad425a207c7204756669a4812
Jaydeep-Shirsath <91476851+Jaydeep-Shirsath@users.noreply.github.com>
2024-12-15 15:33:24 +0530

    Add files via upload

---
+++ scan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+red='\033[31m'
+reset='\033[0m'
+blue='\033[34m'
+
+echo -e "${red}"
+cat << EOF
+ ____            _         ____                                  
+|  _ \ ___  _ __| |_ ___  / ___|  ___ __ _ _ __  _ __   ___ _ __ 
+| |_) / _ \| '__| __/ __| \___ \ / __/ _  | '_ \| '_ \ / _ \ '__|
+|  __/ (_) | |  | |_\__ \  ___) | (_| (_| | | | | | | |  __/ |   
+|_|   \___/|_|   \__|___/ |____/ \___\__,_|_| |_|_| |_|\___|_|   
+                                               
+EOF
+echo -e "${reset}"
+echo -e "${blue}Created by HackTheHacker${reset}"
+echo -e "${blue}https://youtube.com/@js44${reset}"
+
+read -p "Enter IP address: " IP
+nmap -A $IP -p- -v -sV -Pn --open 2>/dev/null
